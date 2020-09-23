c68283597e3377ddbde621322e48ab97760014a3
kenzoh1 <kenzohaddad@gmail.com>
2020-09-23 17:42:20 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import sys
+from mycroft import MycroftSkill, intent_file_handler
+from Adafruit_IO import MQTTClient
+
+ADAFRUIT_IO_KEY = 'aio_VnEL00LvsqUcyaeJMAbh8QO5NfBo'
+ADAFRUIT_IO_USERNAME = 'Kenzo16'
+
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+client.connect()
+client.loop_background()
+
+
+class FanoffControl(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+    @intent_file_handler('fanoff.control.intent')
+    def handle_jarvis_introducing(self, message):
+        self.speak_dialog('fanoff.control')
+        client.publish('Fan', 0)
+
+
+def create_skill():
+    return FanControl()
+
